d30d5dafc81207bc331a5adf349fafbd3148631c
Simon1705 <simonpeter@student.telkomuniversity.ac.id>
2025-01-26 19:24:48 +0700

    remove useMemo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState, useRef, useCallback, useMemo } from 'react';
+import { useEffect, useState, useRef, useCallback } from 'react';
 import Masonry from 'react-masonry-css';
 import { motion, useScroll, useSpring, AnimatePresence } from 'framer-motion';
 import Image from 'next/image';
